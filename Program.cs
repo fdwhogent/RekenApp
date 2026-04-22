85417f67f9518890aea3904e0bee808fe65e05c3
Frederiek DW <frederiek.dewolf@hogent.be>
2026-04-22 15:03:19 +0200

    Verbeter resultaatstring van de optelling

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Voer het tweede getal in:");
             int getal2 = Convert.ToInt32(Console.ReadLine());
             int som = getal1 + getal2;
-            Console.WriteLine("De som is: " + som);
+            Console.WriteLine($"De som van {getal1} en {getal2} is: {som}");
         }
     }
 }
