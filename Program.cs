7944912dc4815388c4d941f0d760e17c942e2f35
Frederiek DW <frederiek.dewolf@hogent.be>
2026-04-22 14:44:59 +0200

    Verwijder argument van Main: fixes #1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace RekenApp {
     internal class Program {
-        static void Main(string[] args) {
+        static void Main() {
             Console.WriteLine("Hello, World!");
         }
     }
