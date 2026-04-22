e7e54117dde3bbc1eb88c1f91b50e7b9f3dbbe45
Frederiek DW <frederiek.dewolf@hogent.be>
2026-04-22 15:17:51 +0200

    Werk vermenigvuldigfeature uit

--- Program.cs
+++ Program.cs
@@ -6,8 +6,18 @@
             int getal1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Voer het tweede getal in:");
             int getal2 = int.Parse(Console.ReadLine());
-            int som = getal1 + getal2;
-            Console.WriteLine($"De som van {getal1} en {getal2} is: {som}");
+
+            Console.WriteLine("Optellen (+) of Vermenigvuldigen (*)?");
+            string actie = Console.ReadLine();
+            if (actie == "+") {
+                int som = getal1 + getal2;
+                Console.WriteLine($"De som van {getal1} en {getal2} is: {som}");
+            } else if (actie == "*") {
+                int product = getal1 * getal2;
+                Console.WriteLine($"Het product van {getal1} en {getal2} is: {product}");
+            } else {
+                Console.WriteLine("Ongeldige actie.");
+            }
         }
     }
 }
