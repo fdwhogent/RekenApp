9e60de416d1819c779cdf9297d48ed1b105535b7
Frederiek DW <frederiek.dewolf@hogent.be>
2026-04-22 15:09:34 +0200

    Wijzig convertiemethode

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@
         static void Main() {
             // vraag om 2 getallen en druk som op de console
             Console.WriteLine("Voer het eerste getal in:");
-            int getal1 = Convert.ToInt32(Console.ReadLine());
+            int getal1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Voer het tweede getal in:");
-            int getal2 = Convert.ToInt32(Console.ReadLine());
+            int getal2 = int.Parse(Console.ReadLine());
             int som = getal1 + getal2;
             Console.WriteLine($"De som van {getal1} en {getal2} is: {som}");
         }
